5da08c5de34be438480f5148798b39649b83cbdf
Ben Cofield <benrcofield@gmail.com>
2022-09-18 14:24:58 -0400

    First Commit

---
+++ MethodsWithLoopsAndConditionals/MethodsWithLoopsAndConditionals/Methods.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithLoopsAndConditionals;
+
+internal class Methods
+{
+
+
+    public static void Print1000toNeg1000()
+    {
+        for (int i = 1000; i > -1000; i--)
+        {
+            Console.Write($"{i} ");
+
+            if (i % 20 == 0) Console.WriteLine("\n");
+        }
+    }
+
+    public static void CountByThrees()
+    {
+        for (int i = 3; i <= 999; i+= 3)
+        {
+            Console.Write($"{i} ");
+
+            if (i % 20 == 0) Console.WriteLine("\n");
+        }
+    }
+
+    public static bool IsEqual(int num1, int num2)
+    {
+        if (num1 == num2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //returns 0 for even;
+    //returns 1 for odd
+    public static int EvenOrOdd(int num)
+    {
+        return num % 2;
+    }
+
+    //returns 1 for Positive
+    //returns -1 for Negative
+    //returns 0 for 0
+    public static int PositiveOrNegative(int num)
+    {
+        if (num > 0) 
+        {
+            return 1;
+        }
+        else if (num < 0) 
+        {
+            return -1;
+
+        }
+        else 
+        {
+            return 0;
+        }
+    }
+
+    public static bool CanVote(int age)
+    {
+        if (age >= 18)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static bool IsInRange(int userInput)
+    {
+        if (userInput >= -10 && userInput <= 10) 
+        { 
+            return true; 
+        }
+        else return false;
+    }
+
+    public static void MultiplicationTables(int num)
+    {
+        for (int i = 1; i <= 12; i++)
+        {
+            Console.WriteLine($"{num} x {i} = {num * i}");
+        }
+    }
+}
---
+++ MethodsWithLoopsAndConditionals/MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MethodsWithLoopsAndConditionals;
+
+namespace MethodsExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            //print all numbers decreasing from 1000 to -1000
+            Methods.Print1000toNeg1000();
+
+            //print multiples of 3 up to 999
+            Methods.CountByThrees();
+
+            //Enter two integers and check if they are equal
+            Console.WriteLine("Enter two integers: ");
+            var input1 = int.Parse(Console.ReadLine());
+            var input2 = int.Parse(Console.ReadLine());
+
+            var output = (Methods.IsEqual(input1, input2)) ? "These numbers are equal" : "These numbers are not equal";
+            Console.WriteLine(output);
+
+            //1. Checks if input number is even or odd
+            //2. Checks if input number is positive or negative
+            Console.WriteLine("Enter one integer: ");
+            input1 = int.Parse(Console.ReadLine());
+
+            output = (Methods.EvenOrOdd(input1) == 0) ? "This number is éven" : "This number is odd";
+            Console.WriteLine(output);
+
+            
+            if (Methods.PositiveOrNegative(input1) == 1)
+            {
+                output = "This number is positive";
+            }
+            else if (Methods.PositiveOrNegative(input1) == -1) 
+            {
+                output = "This number is negative";
+            }
+            else 
+            {
+                output = "This number is 0 thus neither positive or negative";
+            }
+            Console.WriteLine(output);
+
+            //Check if user is old enough to vote
+            Console.Write("Enter your age: ");
+            input1 = int.Parse(Console.ReadLine());
+
+            output = (Methods.CanVote(input1)) ? "You can vote!" : "You are not old enough to vote yet";
+            Console.WriteLine(output);
+
+
+            //Check if user input is within range
+            Console.WriteLine("Enter an integer between -10 and 10: ");
+            input1 = int.Parse(Console.ReadLine());
+            
+            if (Methods.IsInRange(input1))
+            {
+                Console.WriteLine("In range");
+            }
+            else
+            {
+                Console.WriteLine("Error: Not in range");
+            }
+
+            //Print out multiplication table for user input
+            Console.WriteLine("Enter an integer: ");
+            input1 = int.Parse(Console.ReadLine());
+            Methods.MultiplicationTables(input1);
+        }
+    }
+}
